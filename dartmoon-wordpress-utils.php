c4bc9465d7ba19724989fd317c10591a8313c978
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2021-08-18 21:57:54 +0200

    Added dartmoon-wordpress-utils

---
+++ dartmoon-wordpress-utils.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:  Dartmoon WordPress utils 
+ * Plugin URI:   https://dartmoon.io
+ * Description:  Some general functions for wordpress
+ * Version:      1.0.0
+ * Author:       Dartmoon
+ * Author URI:   https://dartmoon.io
+ * License:      PROPRIETARY
+ */
+
+/**
+ * Register the same callable for multiple filters
+ */
+function add_filters($tags, $callable, $priority = 10, $accepted_args = 1) {
+    if (!is_array($tags)) {
+        $tags = [$tags];
+    }
+    
+    foreach ($tags as $tag) {
+        add_filter($tag, $callable, $priority, $accepted_args);
+    }
+}
